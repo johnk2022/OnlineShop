91b2291343f2ece52e3726aeef364092835b4866
johnk2022 <keenan.john@hotmail.com>
2022-09-22 12:13:16 +0000

    added aws

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['adojk.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['adojk.herokuapp.com', 'localhost']
 
 
 # Application definition
